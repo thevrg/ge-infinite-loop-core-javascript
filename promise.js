4234b1d4ffe84e7dfcf2267f4ef24278924623c4
Peter Varga <peter.varga@dpc.hu>
2018-12-06 14:34:18 +0100

    promise.js

--- promise.js
+++ promise.js
@@ -27,11 +27,18 @@ class MyService {
 
     wait(seconds) {
 
-        const promise = new Promise((resolve, reject) => {
+
+        const promiseLogic = (resolve, reject) => {
             setTimeout(() => {
-                resolve({timeElapsed: seconds, message: "Hello, time is out", number: Math.random() * 100});
+                resolve({
+                    timeElapsed: seconds,
+                    message: "Hello, time is out",
+                    number: Math.random() * 100
+                });
             }, seconds * 1000);
-        });
+        };
+
+        const promise = new Promise(promiseLogic);
 
         return promise;
     }
@@ -47,30 +54,38 @@ class MySecondService {
         return this.myService.wait(seconds)
             .then(
                 (success) => {
-                        console.log("resolved");
-                        if(success.number > 50) {
-                            return "succeeded"
-                        } else {
-                            throw "invalid number";
-                        }
-                }
+                    console.log("resolved");
+                    if (success.number > 50) {
+                        return "succeeded"
+                    } else {
+                        throw "invalid number";
+                    }
+                },
             );
     }
 
 }
 
 const mySerice = new MyService();
+
 const mySecondService = new MySecondService(mySerice);
 
 
-
-
-mySecondService.valami(2)
-.then(
-    (success) => {
-        console.log("SUCCESS", success);
-    },
-    (failure) => {
-        console.log("FAILURE", failure);
-    }
-);
+mySecondService.valami(1)
+    .then(
+        (success) => {
+            console.log("SUCCESS", success);
+            throw success;
+        },
+        (failure) => {
+            console.log("FAILURE", failure);
+            return failure;
+        }
+    ).then(
+        (success) => {
+            console.log("SUCCESS2", success);
+        },
+        (failure) => {
+            console.log("FAILURE2", failure);
+        }
+    );
